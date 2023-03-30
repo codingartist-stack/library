e78e015a55696046f900e8ea73a9893446e56561
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-30 16:50:38 -0600

    created the book constructor.

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+let myLibray = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + " by " + author + ", " + pages + " pages, " + read;
+  };
+}
