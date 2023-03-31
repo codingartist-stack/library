ea304013b786c92d175d7d68cbf2084b49f99105
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-03-31 12:06:00 -0600

    used getElementById to grab the fields from the html so we could add them to array.

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,10 @@
 console.log("I am connected to the html file");
 
 const displayForm = document.getElementById("showForm");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("numOfPages");
+const haveRead = document.getElementById("haveRead");
 
 displayForm.addEventListener("click", (e) => {
   document.getElementById("form_container").style.display = "grid";
