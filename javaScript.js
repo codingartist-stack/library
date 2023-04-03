fefe17138506ce564d310fc721ae1be0c95111a9
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-03 12:42:35 -0600

    got the information to display.

--- javaScript.js
+++ javaScript.js
@@ -1,37 +1,37 @@
-console.log("I am connected to the html file");
+console.log('I am connected to the html file');
 
-const displayForm = document.getElementById("showForm");
-const bookTitle = document.getElementById("title");
-const bookAuthor = document.getElementById("author");
-const numOfPages = document.getElementById("numOfPages");
-const haveReadOrNot = document.getElementById("haveRead");
-const submitBook = document.getElementById("submitBook");
-const clearButton = document.getElementById("clear");
-const cardContainer = document.getElementById("card_container");
+const displayForm = document.getElementById('showForm');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const numOfPages = document.getElementById('numOfPages');
+const haveReadOrNot = document.getElementById('haveRead');
+const submitBook = document.getElementById('submitBook');
+const clearButton = document.getElementById('clear');
+const cardContainer = document.getElementById('card_container');
 
-displayForm.addEventListener("click", (e) => {
-  document.getElementById("form_container").style.display = "grid";
+displayForm.addEventListener('click', (e) => {
+  document.getElementById('form_container').style.display = 'grid';
 });
 
 let myLibray = [
-  { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 295, read: "Read" },
+  { title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 295, read: 'Read' },
   {
     title: "Howl's Moving Castle",
-    author: "Diana Wynne Jones",
+    author: 'Diana Wynne Jones',
     pages: 329,
-    read: "Read",
+    read: 'Read',
   },
   {
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
     pages: 214,
-    read: "Read",
+    read: 'Read',
   },
   {
-    title: "The Boy, the Mole, the Fox and the Horse",
-    author: "Charlie Mackesy",
+    title: 'The Boy, the Mole, the Fox and the Horse',
+    author: 'Charlie Mackesy',
     pages: 128,
-    read: "Read",
+    read: 'Read',
   },
 ];
 
@@ -41,7 +41,7 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return title + " by " + author + ", " + pages + " pages, " + read;
+    return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
   };
 }
 
@@ -50,9 +50,9 @@ function addBookToLibrary() {
   author = bookAuthor.value;
   pages = numOfPages.value;
   if (haveReadOrNot.checked) {
-    read = "Read";
+    read = 'Read';
   } else {
-    read = "Have Not Read";
+    read = 'Have Not Read';
   }
 
   let nextBook = new Book(title, author, pages, read);
@@ -61,21 +61,21 @@ function addBookToLibrary() {
 }
 
 function clearForm() {
-  bookTitle.value = "";
-  bookAuthor.value = "";
-  numOfPages.value = "";
+  bookTitle.value = '';
+  bookAuthor.value = '';
+  numOfPages.value = '';
   if (haveReadOrNot.checked) {
     haveReadOrNot.checked = false;
   }
 }
 
-submitBook.addEventListener("click", (e) => {
+submitBook.addEventListener('click', (e) => {
   //   console.log("am I working?");
   addBookToLibrary();
   clearForm();
 });
 
-clearButton.addEventListener("click", (e) => {
+clearButton.addEventListener('click', (e) => {
   clearForm();
 });
 
@@ -83,14 +83,24 @@ function displayCard() {
   //display each book
   //Loop!
   //create div for each book (appendchild)
-  //in the div display info (how will it be displayed?)
-  for (let index = 0; index < myLibray.length; index++) {
-    const bookInfo = document.createElement("div");
-    bookInfo.classList.add("bookDetails");
-    bookInfo.innerHTML =
-      "<strong>Title: </strong> ${title[0]} <br /><strong>Author: </strong> <br /><strong>Pages: </strong> <br />";
+  //in the div display info (how will it be displayed?
+  for (const book of myLibray) {
+    const bookInfo = document.createElement('div');
+    bookInfo.classList.add('bookDetails');
+    bookInfo.innerHTML = `<strong>Title: </strong> ${book.title} 
+    <br /><strong>Author: </strong> ${book.author} 
+    <br /><strong>Pages: </strong> ${book.pages}
+    <br /><strong>Read Status: </strong> ${book.read}`;
     cardContainer.appendChild(bookInfo);
   }
+
+  //   for (let index = 0; index < myLibray.length; index++) {
+  //     const book = myLibray[index];
+  //     const bookInfo = document.createElement('div');
+  //     bookInfo.classList.add('bookDetails');
+  //     bookInfo.innerHTML = `<strong>Title: </strong> ${book.title} <br /><strong>Author: </strong> <br /><strong>Pages: </strong> <br />`;
+  //     cardContainer.appendChild(bookInfo);
+  //   }
 }
 
 displayCard();
